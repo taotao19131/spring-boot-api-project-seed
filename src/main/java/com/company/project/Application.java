9f3d8a9aa5688f3ef8deecdfcbd6d5749ff761ee
taotao19131 <44084221+taotao19131@users.noreply.github.com>
2019-04-19 11:26:14 +0800

    Update Application.java

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -1,9 +1,17 @@
 package com.company.project;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
+import com.taotao.project.core.Mapper;
+import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableSwagger2Doc
+@EnableTransactionManagement
+@MapperScan(value = "com.taotao.project.dao", markerInterface = Mapper.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
